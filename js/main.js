2f6f9077216be11ed68c5fd9ceaef6781548b257
kovalyova-alena <Nfyrjgbz17>
2017-05-26 12:23:47 +0300

    nav menu with flexBox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-/**
- * Created by Alena on 10.04.2017.
- */
+(function() {
+    var toggle = document.querySelector("#flexy-nav__toggle");
+    var nav = document.querySelector("#flexy-nav__items");
+    toggle.addEventListener("click", function(e) {
+        e.preventDefault();
+        nav.classList.contains("flexy-nav__items--visible") ? nav.classList.remove("flexy-nav__items--visible") : nav.classList.add("flexy-nav__items--visible");
+    });
+})();
