1b5685fb6a401572d054cb139438ee43c543ccf1
kovalyova-alena <Nfyrjgbz17>
2017-05-30 13:25:30 +0300

    desktop and mobile versiona are created

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(document).ready(function (e){
-    
 
-      $('.nav').on('mouseover', '.item-menu', function (){
-          $('.nav>li>a:focus').blur();
-          $(this).addClass('open');
-      });
+    openMenuOnHover();
 
-    $('.nav').on('mouseout', '.item-menu', function (){
-        $(this).removeClass('open');
+    $('.navbar-toggle').on('click', function () {
+        $('.navbar').toggleClass('nav-opened');
+    });
+
+    $(window).resize(function(){
+            openMenuOnHover();
     });
 
 
+
+    function openMenuOnHover() {
+        if ($(window).width() > 767) {
+            $('.nav').on('mouseenter', '.item-menu', function (){
+                $('.nav>li>a:focus').blur();
+                $(this).addClass('open');
+            });
+
+            $('.nav').on('mouseleave', '.item-menu', function (){
+                $(this).removeClass('open');
+            });
+        }
+    }
 });
