7bd847bade9dc4d1078e5450241259c430f7a770
kovalyova-alena <Nfyrjgbz17>
2017-06-02 12:37:57 +0300

    create section Tabs without content in Tab Price

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function (e){
 
     //Menu onHover
     openMenuOnHover();
+    bx();
 
     //bxSlider
     $('.bxslider').bxSlider({
@@ -12,14 +13,42 @@ $(document).ready(function (e){
     });
 
 
+    //FancyBox
+    $("[data-fancybox]").fancybox({
+        loop : true,
+        infobar : false,
+        thumbs : false,
+        image : {
+            protect: true
+        },
+        zoomIn : false
 
+    });
+
+    
     $('.navbar-toggle').on('click', function () {
         $('body').toggleClass('nav-opened');
     });
 
     $(window).resize(function(){
             openMenuOnHover();
+            bx();
     });
+
+    function bx() {
+        var windowW = $(window).width();
+        var quantity;
+        quantity = (windowW < 768) ? 2 : 5;
+        console.log(quantity);
+        $('.sliderCarousel').bxSlider({
+            slideWidth: windowW,
+            minSlides: quantity,
+            maxSlides: 5,
+            moveSlides: 1,
+            slideMargin: 0
+        });
+    }
+
 
     function openMenuOnHover() {
         if ($(window).width() > 767) {
